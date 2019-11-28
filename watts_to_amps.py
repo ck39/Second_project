642ffb89970a9045721b04d2fc089fb30c301af5
ck39 <50014809+ck39@users.noreply.github.com>
2019-11-28 22:23:35 +0000

    Changed word casing to be consistent.

--- watts_to_amps.py
+++ watts_to_amps.py
@@ -3,12 +3,12 @@ import math
 # currentType must be one of the following strings: DC / Single phase AC / Three phase AC
 def wattsToAmps(currentType):
     currentType = currentType.lower()
-    power = float(input('Enter power in watts: '))   
+    power = float(input('Enter power in Watts: '))   
     if currentType == 'single phase ac' or currentType == 'three phase ac':
         powerFactor = float(input('Enter power factor: '))   
     if currentType == 'three phase ac':
         voltageType = input('Enter voltage type (line-to-line or line-to-neutral: ').lower()        
-    voltage = float(input('Enter voltage in volts: '))
+    voltage = float(input('Enter voltage in Volts: '))
     
     if currentType == 'dc':
         amps = power / voltage
